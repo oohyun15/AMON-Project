05ee0111432955ea964197886431d46c89f7775c
oohyun15 <sakiss@ajou.ac.kr>
2019-07-09 03:30:42 +0900

    Add Fog
    캐릭터 scale: 5 -> 25
    맵 scale: 10 -> 100
    카메라 앵글 수정
    안개 plane은 ProBuilder 에셋 사용

---
+++ Assets/Scripts/FogOfWar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+    public GameObject FogPlane;
+    public Transform Character;
+    public LayerMask FogLayer;
+    public float radius;
+    public float radiusSqr { get { return radius * radius; } }
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private Color[] colors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r = new Ray(transform.position, Character.position - transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(r, out hit, 1000, FogLayer, QueryTriggerInteraction.Collide))
+        {
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Vector3 v = FogPlane.transform.TransformPoint(vertices[i]);
+
+                float dist = Vector3.SqrMagnitude(v - hit.point);
+
+                if (dist < radiusSqr)
+                {
+                    float alpha = Mathf.Min(colors[i].a, dist / radiusSqr);
+
+                    colors[i].a = alpha;
+                }
+            }
+            UpdateColor();
+        }
+    }
+
+    void Initialize()
+    {
+        mesh = FogPlane.GetComponent<MeshFilter>().mesh;
+
+        vertices = mesh.vertices;
+
+        colors = new Color[vertices.Length];
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = Color.black;
+        }
+        UpdateColor();
+    }
+
+    void UpdateColor()
+    {
+        mesh.colors = colors;
+    }
+}
