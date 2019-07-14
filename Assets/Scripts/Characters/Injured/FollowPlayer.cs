e012775f20800ff1557d2ba865e5f40cd52b6dbd
ooooorobo <40057032+ooooorobo@users.noreply.github.com>
2019-07-14 20:14:00 +0900

    Revert "Add comment"
    This reverts commit 11737f97c3b9be562fc7484dc3d02239e0633e91.

--- Assets/Scripts/Characters/Injured/FollowPlayer.cs
+++ Assets/Scripts/Characters/Injured/FollowPlayer.cs
@@ -1,12 +1,4 @@
-﻿/****************************************
- * FollowPlayer.cs
- * 제작: 조예진
- * 설정된 target을 따라다니도록 하는 코드
- * MinorInjured 오브젝트가 구조되었을 때 플레이어를 따라다니도록 함
- * 작성일자: 19.07.11
- ***************************************/
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/Scripts/Characters/Injured/SeriousInjured.cs
+++ Assets/Scripts/Characters/Injured/SeriousInjured.cs
@@ -1,11 +1,4 @@
-﻿/****************************************
- * SeriousInjured.cs
- * 제작: 조예진
- * 중상 부상자 캐릭터의 상세 상호작용 코드
- * 작성일자: 19.07.11
- ***************************************/
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
