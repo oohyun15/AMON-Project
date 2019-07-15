225cb76607c8ae5eba2b85bf792efa4a34a322eb
oohyun15 <sakiss@ajou.ac.kr>
2019-07-15 23:48:35 +0900

    Add Comment
    FogOfWar 스크립트에 주석 추가

--- Assets/Scripts/Maps/Fog/FogOfWar.cs
+++ Assets/Scripts/Maps/Fog/FogOfWar.cs
@@ -1,32 +1,47 @@
-﻿using System.Collections;
+﻿/****************************************
+ * FogOfWar.cs
+ * 제작: 김용현
+ * 스테이지 내 전장의 안개를 구현한 코드로 캐릭터 위치에 따라 안개 plane의 alpha값을 변경하는 코드
+ * https://www.youtube.com/watch?v=iGAdaZ1ICaI 참고
+ * 함수 추가 및 수정 시 누가 작성했는지 꼭 해당 함수 주석으로 명시해주세요!
+ * 작성일자: 19.07.09
+ ***************************************/
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FogOfWar : MonoBehaviour
 {
-    public GameObject FogPlane;
-    public Transform Character;
-    public LayerMask FogLayer;
-    public float radius;
-    public float radiusSqr { get { return radius * radius; } }
+    public GameObject FogPlane;             // 안개 Plane
+    public Transform Character;             // 캐릭터 좌표(Transform) 변수
+    public LayerMask FogLayer;              // 안개의 LayerMask 변수
+    public float radius;                    // 캐릭터 중심으로 밝혀질 원의 반지름
+    public float radiusSqr { get { return radius * radius; } }  // 반지름 제곱
 
-    private Mesh mesh;
-    private Vector3[] vertices;
-    private Color[] colors;
+    private Mesh mesh;                      // 안개 Plane의 Mesh 컴포넌트를 받기위한 변수
+    private Vector3[] vertices;             // 안개에서 보여질 위치(벡터)값
+    private Color[] colors;                 // 안개의 색깔 변수
 
     // Start is called before the first frame update
     void Start()
     {
+        // 게임 시작 시 안개 게임오브젝트에 대해 초기화
         Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Character != null) // 캐릭터 사망 시 오류뜨는거 방지용 if문
+        // 캐릭터 사망 시 오류뜨는거 방지용 if문. 김태윤 추가
+        if (Character != null) 
         {
             Ray r = new Ray(transform.position, Character.position - transform.position);
+
             RaycastHit hit;
+
+            // 캐릭터 중심으로 radius만큼 시야가 밝혀짐
             if (Physics.Raycast(r, out hit, 1000, FogLayer, QueryTriggerInteraction.Collide))
             {
                 for (int i = 0; i < vertices.Length; i++)
@@ -44,11 +59,10 @@ public class FogOfWar : MonoBehaviour
                 }
                 UpdateColor();
             }
-
         }
-      
     }
 
+    // 안개 초기화 함수
     void Initialize()
     {
         mesh = FogPlane.GetComponent<MeshFilter>().mesh;
@@ -64,6 +78,7 @@ public class FogOfWar : MonoBehaviour
         UpdateColor();
     }
 
+    // 안개 Mesh를 업데이트하는 함수
     void UpdateColor()
     {
         mesh.colors = colors;
