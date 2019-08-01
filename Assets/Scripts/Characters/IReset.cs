1aa5db57d49c78465d855200e45fe1a091cae772
oohyun15 <sakiss@ajou.ac.kr>
2019-08-01 18:23:37 +0900

    Fix minor bug
    파일명 수정(IChrarcter -> IReset)

--- Assets/Scripts/Characters/IReset.cs
+++ Assets/Scripts/Characters/IReset.cs
@@ -1,14 +1,9 @@
 ﻿/****************************************
- * ICharacter.cs
+ * IReset.cs
  * 제작: 김용현
  * 게임 내 공통 캐릭터 특징을 가지는 스크립트
  * 작성일자: 19.08.01
  ***************************************/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 
 interface IReset
 {
@@ -17,5 +12,4 @@ interface IReset
 
     // 초기값 설정
     void SetInitValue();
-
 }
