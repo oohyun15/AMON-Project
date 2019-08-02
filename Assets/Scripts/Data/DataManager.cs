8a05569bbd6bc49464a4fd7cedc6957a1b4f794f
ooooorobo <40057032+ooooorobo@users.noreply.github.com>
2019-08-03 01:41:59 +0900

    Add UserDataIO.cs
    유저 데이터를 xml로 관리하는 스크립트 추가
    - 우선은 확인하기 편하도록 Assets/Data 폴더 안에 생성되도록 설정
    
    데이터 관련 스크립트 Data 폴더로 이동

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -3,7 +3,9 @@
  * 제작: 조예진
  * 게임 데이터 로드 및 저장 스크립트
  * 함수 추가 및 수정 시 누가 작성했는지 꼭 해당 함수 주석으로 명시해주세요!
+ * (19.08.02) 유저 데이터 xml에서 불러오고 저장하는 부분 추가
  * 작성일자: 19.07.30
+ * 수정일자: 19.08.02
  ***************************************/
 
 using System.Collections;
@@ -51,9 +53,17 @@ public class DataManager : MonoBehaviour
     private int maxLeftToMiddleCondition;
     public int MaxLeftToMiddleCondition { get { return maxLeftToMiddleCondition; } }
 
+    private UserDataIO.User user;
+
+    public UserDataIO.User GetUser() { return user; }
+
     private void Awake()
     {
         LoadStageData();
+
+        // xml 이용한 로딩
+        user = UserDataIO.ReadUserData();
+        UserDataIO.WriteUserData(user);     // 임시 테스트
     }
 
     // 스테이지 데이터 로드
@@ -84,8 +94,15 @@ public class DataManager : MonoBehaviour
 
     public void SaveGameResult(int money, int honor)
     {
+        // PlayerPrefs 이용한 저장
         PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money", 0) + money);
         PlayerPrefs.SetInt("Honor", PlayerPrefs.GetInt("Honor", 0) + honor);
+
+        // (19. 08. 02) xml 이용한 저장
+        user.honor += honor;
+        user.money += money;
+
+        UserDataIO.WriteUserData(user);
     }
 
 
---
+++ Assets/Scripts/Data/UserDataIO.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+
+public class UserDataIO : MonoBehaviour
+{
+    // 유저 데이터로 저장할 값 클래스
+    public class User
+    {
+        public int money;       // 소지 금액
+        public int honor;       // 소지 명예 점수
+        // 저장할 값 늘어날 경우, WriteUserData와 ReadUserData에서 Set/GetAttribute 설정해 주어야 합니다
+    }
+
+    private static string userDataFileName = "/userData.xml";
+
+    // 유저 데이터 저장 시
+    public static void WriteUserData(User user)
+    {
+        string path = GetPath(userDataFileName);
+
+        XmlDocument doc = new XmlDocument();
+        XmlElement userElement = doc.CreateElement("user");
+
+        userElement.SetAttribute("money", user.money.ToString());
+        userElement.SetAttribute("honor", user.honor.ToString());
+
+        doc.AppendChild(userElement);
+        doc.Save(path);
+    }
+
+    public static User ReadUserData()
+    {
+        string path = GetPath(userDataFileName);
+
+        User user = new User();
+
+        if (!File.Exists(path))
+        {
+            user = new User
+            {
+                money = 0,
+                honor = 0
+            };
+            WriteUserData(user);
+        }
+        else
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            XmlElement userElement = doc["user"];
+
+            user = new User
+            {
+                money = System.Convert.ToInt32(userElement.GetAttribute("money")),
+                honor = System.Convert.ToInt32(userElement.GetAttribute("honor"))
+            };
+        }
+
+        return user;
+    }
+
+    private static string GetPath(string fileName)
+    {
+        string path = Application.dataPath + "/Data";       // Assets 경로 안에 저장됨 (테스트)
+        DirectoryInfo di = new DirectoryInfo(path);
+
+        // 경로 존재하는지 확인하고 Data 폴더 없으면 생성
+        if (!di.Exists)
+            di.Create();
+        // Application.persistentDataPath;                  // --> 로컬 저장소에 저장
+
+        path += fileName;
+
+        Debug.Log(fileName + " 저장 경로 : " + path);
+
+        return path;
+    }
+}
