43a3c55b31da977efcdada0b41481908ef09dd48
gost996 <gost996@naver.com>
2019-10-03 03:34:00 +0900

    Fix Attacking Way
    AmonController
    - Destroy 관련 스크립트 전부 수정
    - DestroyObs 삭제 및 CalculateDamage 추가
    - 애니메이션 적당한 부분에 CalculateDamage를 넣어서 아다리맞게 깔끔하게 장애물 파괴
    
    ButtonImageChane 스크립트 추가
    - 구조 여부에 따라 발차기 / 도끼질 버튼 바뀌도록 설정

---
+++ Assets/Scripts/UI/ButtonImageChange.cs
@@ -0,0 +1,38 @@
+﻿/****************************************
+ * ButtonImageCHange.cs
+ * 제작: 김태윤
+ * InteractionButton 이미지 교체하는 스크립트
+ * 작성일자: 19.07.09
+ * 수정일자: 19.08.07
+ ***************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonImageChange : MonoBehaviour
+{
+    public GameObject axeImage;
+    public GameObject kickImage;
+
+    private bool _isRescuing = false;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        _isRescuing = GameManager.Instance.player.isRescuing;
+        if (_isRescuing)
+        {
+            if (axeImage.activeInHierarchy) axeImage.SetActive(false);
+
+            kickImage.SetActive(true);
+        }
+        else
+        {
+            if (kickImage.activeInHierarchy) kickImage.SetActive(false);
+
+            axeImage.SetActive(true);
+        }
+    }
+}
