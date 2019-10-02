38c91bbdee45a89b59223d48cf558e3fee1efe01
gost996 <gost996@naver.com>
2019-10-03 05:32:32 +0900

    Add StreeSlider
    피로도 Slider 추가

---
+++ Assets/Scripts/UI/StressSlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StressSlider : MonoBehaviour
+{
+    private Image stressSlider;
+
+    private UserDataIO.User user;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stressSlider = GetComponent<Image>();
+        user = UserDataIO.ReadUserData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stressSlider.fillAmount = user.stress * 0.01f;
+    }
+}
