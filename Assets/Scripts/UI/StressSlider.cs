92a7bec20bb333160aa277539711a8afdc93338f
ooooorobo <40057032+ooooorobo@users.noreply.github.com>
2019-10-10 02:12:24 +0900

    Change UI sprites
    > stage01
    - 결과창 보상 목록에 피로도 추가 + 재도전 버튼 삭제
    (Fail 스프라이트 변경은 스크립트 수정 필요해 잠시 보류)
    - 인터랙션 버튼 UI 변경
    - 설정창 재도전 버튼 조작방법 버튼으로 변경 (기능 설정 안됨)
    - 미니맵 스프라이트 변경
    - 미니맵 크기 틀에 맞춤
    - 미니맵 배경 스프라이트 이쁘게 교체
    
    > Lobby
    - Top btn UI 길이 늘림
    - 피로도 UI 위치 이동, UI 변경
    - 게임 시작 텍스트 변경 Game Start --> 화재 현장 출동
    
    - Settings 폴더 Setting_old로 이름 변경 후 Setting 폴더 안으로 이동

--- Assets/Scripts/UI/StressSlider.cs
+++ Assets/Scripts/UI/StressSlider.cs
@@ -6,14 +6,21 @@ using UnityEngine.UI;
 public class StressSlider : MonoBehaviour
 {
     private Image stressSlider;
+    private Text stressText;
 
     private UserDataIO.User user;
 
     // Start is called before the first frame update
+    // (19.10.10.예진) 피로도 텍스트 설정
     void Start()
     {
         stressSlider = GetComponent<Image>();
+        stressText = transform.GetChild(0).GetComponent<Text>();
+
         user = UserDataIO.ReadUserData();
+
+        stressText.text = user.stress + "%";
+
     }
 
     // Update is called once per frame
