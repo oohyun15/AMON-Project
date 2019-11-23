3b1a9d11cb6fc749412d74a19c03ef44868d5036
ooooorobo <40057032+ooooorobo@users.noreply.github.com>
2019-11-24 00:02:13 +0900

    Build ver 0.5.3a
    세팅만 변경함 빌드 실패

---
+++ Assets/Scripts/CinematicCameraRot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicCameraRot : MonoBehaviour
+{
+    public GameObject center;
+
+    void Start()
+    {
+        StartCoroutine(rot());
+    }
+
+
+    IEnumerator rot()
+    {
+        while (true)
+        {
+            gameObject.transform.RotateAround(center.transform.position, 
+                new Vector3(0,1,0), 
+                Time.deltaTime * 50);
+
+            yield return null;
+        }
+    }
+}
