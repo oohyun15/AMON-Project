8bad750eedb3b3375284262051280bbcdc99bf0e
ooooorobo <40057032+ooooorobo@users.noreply.github.com>
2019-12-03 00:01:39 +0900

    Add credit scene
    테스트 씬 폴더에 크레딧 씬 추가
    크레딧 올라가는 것과 터치하면 속도 빨라지는 것까지 구현

---
+++ Assets/Scripts/UI/Credit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    bool isTouched;
+    public int speed;
+
+    private void Start()
+    {
+        isTouched = false;
+
+        StartCoroutine(StartCredit());
+    }
+
+    IEnumerator StartCredit()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0)) speed *= 2;
+            else if (Input.GetMouseButtonUp(0)) speed /= 2;
+
+            transform.Translate(new Vector2(0, Time.deltaTime * speed));
+
+            yield return null;
+        }
+    }
+
+    public void MoveScene(string scene)
+    {
+        Lobby.MoveScene(scene);
+    }
+}
