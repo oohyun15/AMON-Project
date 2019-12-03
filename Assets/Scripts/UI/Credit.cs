60e15830b87218449a13f43921e1b1aa9816a3f7
ooooorobo <40057032+ooooorobo@users.noreply.github.com>
2019-12-03 15:16:08 +0900

    Update credit scene
    크레딧 씬 수정

--- Assets/Scripts/UI/Credit.cs
+++ Assets/Scripts/UI/Credit.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Credit : MonoBehaviour
 {
     bool isTouched;
     public int speed;
+    private float bottom;
+    public Text txtTouch;
+    public GameObject btnTouch;
 
     private void Start()
     {
         isTouched = false;
+
+        bottom = transform.parent.position.y 
+            - transform.parent.GetComponent<RectTransform>().rect.height / 2;
 
         StartCoroutine(StartCredit());
     }
 
     IEnumerator StartCredit()
     {
-        while (true)
+        float height = GetComponent<RectTransform>().rect.height / 2;
+        
+        while (transform.position.y - height < bottom)
         {
             if (Input.GetMouseButtonDown(0)) speed *= 2;
             else if (Input.GetMouseButtonUp(0)) speed /= 2;
@@ -25,6 +34,24 @@ public class Credit : MonoBehaviour
 
             yield return null;
         }
+
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        Vector4 tempColor;
+
+        while (txtTouch.color.a < 1)
+        {
+            tempColor = txtTouch.color;
+            tempColor.w += Time.deltaTime * 3;
+            txtTouch.color = tempColor;
+
+            yield return null;
+        }
+
+        btnTouch.SetActive(true);
     }
 
     public void MoveScene(string scene)
